356ff36640ba3e59d6340cac430419908b3704dd
monk <rocket567@duck.com>
2026-01-13 18:28:32 +0100

    Refactor: established modular directory structure and added architectural skeletons

---
+++ src/app/main.cpp
@@ -0,0 +1,23 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+using namespace Qt::StringLiterals;
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    // Самый надежный способ для Qt 6: загрузка через URI модуля и имя файла
+    const QUrl url(u"qrc:/qt/qml/Hogrin/src/ui/qml/Main.qml"_s);
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+
+    engine.load(url);
+
+    return app.exec();
+}
