52d47c04302f326d52ff48fb954f13af08b94395
monk <rocket567@duck.com>
2026-03-03 13:31:59 +0100

    fix: handshake token via sendMessage, bidirectional messaging working

--- src/common/config.h
+++ src/common/config.h
@@ -1,18 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-
 #include <QByteArray>
 
 namespace Config {
-constexpr const char* DB_HOST     = "100.114.164.127";
-constexpr int         DB_PORT     = 5433;
-constexpr const char* DB_NAME     = "hogrin";
-constexpr const char* DB_USER     = "hogrin_app";
-constexpr const char* DB_PASSWORD = "OuMtKpeqC3zt8o+IqQgK";
+constexpr const char* DB_HOST      = "100.114.164.127";
+constexpr int         DB_PORT      = 5433;
+constexpr const char* DB_NAME      = "hogrin";
+constexpr const char* DB_USER      = "hogrin_app";
+constexpr const char* DB_PASSWORD  = "OuMtKpeqC3zt8o+IqQgK";
 
-constexpr const char* BOOTSTRAP_SERVER = "YOUR_SERVER_IP";
+constexpr const char* BOOTSTRAP_SERVER = "2a02:8109:b224:e500:6e4b:90ff:feb9:8c7";
 constexpr quint16     BOOTSTRAP_PORT   = 47832;
-constexpr const char* HANDSHAKE_TOKEN  = "YOUR_TOKEN";
+constexpr const char* HANDSHAKE_TOKEN  = "Y)qrLkv;SfuQ~!7";
 }
 
 #endif // CONFIG_H
